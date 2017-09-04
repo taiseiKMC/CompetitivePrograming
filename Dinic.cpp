287692a278490abb65939caa665f7c96f0509ef1
taiseiKMC <taisei@kmc.gr.jp>
2017-09-04 13:01:36 +0900

    Update ford-fulkerson dinic

--- Dinic.cpp
+++ Dinic.cpp
@@ -1,30 +1,34 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-typedef long long FLOW;
-const FLOW INF=(FLOW)(1e6)*(FLOW)(1e6);
-
-const int N=10000;
+using FLOW = long long;
 struct Edge
 {
     int to;
     FLOW cap;
     int rev;
 };
-vector<Edge> graph[N];
-int iter[N];
-int level[N];
-void add_Edge(int from, int to, FLOW cap)
+class FlowNetwork
+{
+    public:
+        FlowNetwork(int n):graph(vector<vector<Edge>>(n)),iter(vector<int>(n)),level(vector<int>(n))
+        {}
+        void add_Edge(int from, int to, FLOW cap);
+        FLOW dinic(int from, int to);
+
+    private:
+        vector<vector<Edge>> graph;
+        vector<int> iter, level;
+        FLOW dfs(int from, int to, FLOW cap);
+        void bfs(int from);
+};
+void FlowNetwork::add_Edge(int from, int to, FLOW cap)
 {
     //cout<<from<<":"<<to<<" "<<cap<<endl;
 	graph[from].push_back((Edge){to,cap,(int)graph[to].size()});
 	graph[to].push_back((Edge){from,0,(int)graph[from].size()-1});
 }
 
-void bfs(int from)
+void FlowNetwork::bfs(int from)
 {
-    memset(level, -1, sizeof(level));
+    fill(level.begin(),level.end(),-1);
     queue<int> que;
     level[from]=0;
     que.push(from);
@@ -44,7 +48,7 @@ void bfs(int from)
     }
 }
 
-FLOW dfs(int from, int to, FLOW f)
+FLOW FlowNetwork::dfs(int from, int to, FLOW f)
 {
 	if(from == to) return f;
 	for(int &i=iter[from];i<graph[from].size();i++)
@@ -52,7 +56,7 @@ FLOW dfs(int from, int to, FLOW f)
 		Edge &e=graph[from][i];
         if(e.cap > 0 && level[from] < level[e.to])
         {
-            FLOW d = dfs(e.to, to, min(e.cap, f));
+            LL d = dfs(e.to, to, min(e.cap, f));
             if(d>0)
             {
                 e.cap -= d;
@@ -63,14 +67,14 @@ FLOW dfs(int from, int to, FLOW f)
 	}
 	return 0;
 }
-FLOW dinic(int from, int to)
+FLOW FlowNetwork::dinic(int from, int to)
 {
     FLOW flow=0;
 	while(1)
 	{
         bfs(from);
         if(level[to]<0) return flow;
-		memset(iter, 0, sizeof(iter));
+        fill(iter.begin(),iter.end(),0);
         FLOW f;
         while((f=dfs(from,to,INF))>0)
         {
--- Ford-Funkerson.cpp
+++ Ford-Funkerson.cpp
@@ -1,28 +1,27 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-typedef long long LL;
-typedef pair<int, int> PII;
-typedef long double LD;
-
-#define FOR(i,a,b) for(int i=(a);i<(b);++i)
-#define REP(i,n)  FOR(i,0,n)
-#define CLR(a) memset((a), 0 ,sizeof(a))
-
-const int N=101;
 const int INF=1e9;
 struct Edge
 {
 	int from,to,cost,rev;
 };
-vector<Edge> graph[N];
-bool used[101];
-void add_Edge(int from, int to, int cap)
+class FlowNetwork
+{
+    public:
+        FlowNetwork(int n):graph(vector<vector<Edge>>(n)),used(vector<char>(n))
+        {}
+        void add_Edge(int from, int to, int cap);
+        int ford_fulkerson(int from, int to);
+
+    private:
+        vector<vector<Edge>> graph;
+        vector<char> used;
+        int dfs(int from, int to, int cap);
+};
+void FlowNetwork::add_Edge(int from, int to, int cap)
 {
 	graph[from].push_back((Edge){from,to,cap,(int)graph[to].size()});
 	graph[to].push_back((Edge){to,from,0,(int)graph[from].size()-1});
 }
-int dfs(int from, int to, int f)
+int FlowNetwork::dfs(int from, int to, int f)
 {
 	if(from == to) return f;
 	int d;
@@ -41,12 +40,12 @@ int dfs(int from, int to, int f)
 	}
 	return 0;
 }
-int ford_fulkerson(int from, int to)
+int FlowNetwork::ford_fulkerson(int from, int to)
 {
 	int flow=0;
 	while(1)
 	{
-		memset(used,0,sizeof(used));
+        fill(used.begin(),used.end(),0);
 		int f=dfs(from,to,INF);
 		if(f==0)break;
 		flow += f;
