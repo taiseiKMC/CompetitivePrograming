cbc4a327fd48e000e8970393fdee602411db800a
taiseiKMC <taisei@kmc.gr.jp>
2018-11-26 03:42:02 +0900

    Update Garner

--- Garner.cpp
+++ Garner.cpp
@@ -5,6 +5,10 @@ namespace Garner
     https://qiita.com/drken/items/ae02240cd1f8edfc86fd
     */
     using LL = long long;
+	LL gcd(const LL a, const LL b)
+	{
+		return __gcd(a, b);
+	}
     LL mod(const LL p, const LL m)
     {
         return (p<0)?(p%m+m):(p%m);
@@ -28,6 +32,38 @@ namespace Garner
         extgcd(a, m, x, y);
         return mod(x, m);
     }
+
+	/*
+	Garnerの前処理
+	modが互いに素になるようにするor解が無いときは-1を返す
+	*/
+	vector<pair<LL,LL>> normalize(const vector<pair<LL, LL>> &mods)
+	{
+		vector<pair<LL, LL>> ret(mods);
+		for(size_t i=0;i<ret.size();i++)
+		for(size_t j=0;j<i;j++)
+		{
+			LL g=gcd(ret[i].second, ret[j].second);
+			if((ret[i].first - ret[j].first)%g != 0) return vector<pair<LL, LL>>(0);
+			ret[i].second/=g;
+			ret[j].second/=g;
+			LL gi = gcd(ret[i].second, g);
+			LL gj = g/gi;
+			g = gcd(gi, gj);
+			while(g != 1)
+			{
+				gi*=g;
+				gj/=g;
+				g = gcd(gi, gj);
+			}
+			ret[i].second*=gi;
+			ret[j].second*=gj;
+			ret[i].first%=ret[i].second;
+			ret[j].first%=ret[j].second;
+		}
+		return ret;
+	}
+
     LL Garner(const vector<pair<LL, LL>> &mods, const LL m)
     {
         const int n=mods.size();
@@ -51,4 +87,11 @@ namespace Garner
         }
         return p[n];
     }
+
+	LL solve(const vector<pair<LL, LL>> &mods, const LL m)
+	{
+		const vector<pair<LL,LL>> tmp(normalize(mods));
+		if(tmp.size() == 0) return -1;
+		return Garner(tmp, m);
+	}
 }
